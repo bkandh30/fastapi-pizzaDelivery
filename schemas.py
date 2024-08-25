a00e7093c14e8147785c44f47f61ad89cc1a39d6
bkandh30 <kandhari.bhavya20@gmail.com>
2024-08-24 17:59:03 -0700

    added order route

--- schemas.py
+++ schemas.py
@@ -27,3 +27,30 @@ class Settings(BaseModel):
 class LoginModel(BaseModel):
     username:str
     password:str
+
+class OrderModel(BaseModel):
+    id:Optional[int]
+    quantity:int
+    order_status:Optional[str]="PENDING"
+    pizza_size:Optional[str]="SMALL"
+    user_id:Optional[int]
+
+    class Config:
+        orm_mode=True
+        schema_extra={
+            "example":{
+                "quantity":2,
+                "pizza_size":"LARGE"
+            }
+        }
+
+class OrderStatusModel(BaseModel):
+    order_status:Optional[str]="PENDING"
+
+    class Config:
+        orm_mode=True
+        schema_extra={
+            "example":{
+                "order_status":"PENDING"
+            }
+        }
